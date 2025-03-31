d844c79ae92d607ad688574813e803b7034af26f
kimpors <mikita_korshak@ukr.net>
2025-03-31 16:11:40 +0300

    Add style to buttons in inputdialog
    Signed-off-by: kimpors <mikita_korshak@ukr.net>

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,15 +2,6 @@
 using System.Text;
 using System.Windows;
 using Newtonsoft.Json;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Quantify;
 using System.IO;
 
 namespace Quantify;
