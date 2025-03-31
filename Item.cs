dee676a229ef8c68f6f6eaa2ae4714eefd07f87d
kimpors <mikita_korshak@ukr.net>
2025-03-31 13:02:41 +0300

    Add interface to class
    for rename capabilities
    
    Signed-off-by: kimpors <mikita_korshak@ukr.net>

--- Item.cs
+++ Item.cs
@@ -1,7 +1,41 @@
+using System.ComponentModel;
 namespace Quantify;
 
-public class Item
+public class Item : INotifyPropertyChanged
 {
-    public string Name { get; set; }
-    public int Count { get; set; }
+    private string _name;
+    private int _count;
+
+    public string Name 
+    { 
+        get { return _name; }
+        set
+        {
+            if (_name != value)
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+    }
+    public int Count 
+    { 
+        get { return _count; }
+        set
+        {
+            if (_count != value)
+            {
+                _count = value;
+                OnPropertyChanged(nameof(Count));
+            }
+        }
+
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 }
